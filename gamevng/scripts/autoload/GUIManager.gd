5c37905a1d7682b0c7809a023d74568c7db8c859
Vo Hong Luong <luong25105@gmail.com>
2025-11-19 02:40:49 +0700

    fix: resolved conflict

--- gamevng/scripts/autoload/GUIManager.gd
+++ gamevng/scripts/autoload/GUIManager.gd
@@ -28,11 +28,17 @@ func _on_animation_finished(anim_name):
 		emit_signal("fade_from_black_finished")
 
 func set_max_heart_gui(max :int):
+	if max > 10:
+		print("wtf max health = %d??" %max)
+		return
 	_heart_container.set_max_heart(max)
 
 func update_heart_gui(health: int):
+
 	var hearts = _heart_container.get_children()
-	
+	if hearts.size() == 0:
+		return
+		
 	for i in range(health):
 		hearts[i].update(true)
 		
--- gamevng/scripts/autoload/game_manager.gd
+++ gamevng/scripts/autoload/game_manager.gd
@@ -163,7 +163,7 @@ func respawn_at_ground_checkpoint():
 	
 	if player.health <= 0 :
 		return
-	SceneTransition.fade_from_black()
+	GUIManager.fade_from_black()
 	player.lock_input(0.3)
 	player.global_position = _last_ground_checkpoint.global_position
 
