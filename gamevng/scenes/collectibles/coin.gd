c14ad47cb56492acdff002faded9c90b2959fc5d
VoAnKhoi2005 <voankhoi@gmail.com>
2025-12-06 11:40:02 +0700

    feat: add gold to level

--- gamevng/scenes/collectibles/coin.gd
+++ gamevng/scenes/collectibles/coin.gd
@@ -8,16 +8,12 @@ func _ready() -> void:
 	animated_sprite_2d.animation_finished.connect(_on_animation_finished)
 
 func _on_collect():
-	# Ngắt kết nối tương tác để không nhặt được nữa (nếu cần)
-	monitoring = false # Tắt theo dõi va chạm (nếu là Area2D)
-
-	# In ra thông báo thu thập coin
+	monitoring = false
 	print("Đã thu thập " + str(coin_amount) + " coin!")
-	
-	# BẮT ĐẦU CHẠY ANIMATION
 	animated_sprite_2d.play("collected")
 	
-# HÀM MỚI: Chỉ tự hủy khi animation CHẠY XONG
+	GameManager.inventory_system.add_coin(coin_amount)
+
 func _on_animation_finished():
 	if animated_sprite_2d.animation == "collected":
 		queue_free() # Tự hủy sau khi animation "collected" chạy xong
