e79e2c0123450dd4b54f433389b1d5891e48098c
Vo Hong Luong <luong25105@gmail.com>
2025-12-15 12:25:28 +0700

    feat: done lock input player

--- gamevng/scripts/inventory_system.gd
+++ gamevng/scripts/inventory_system.gd
@@ -11,7 +11,9 @@ signal inventory_updated(slot_index: int, texture: Texture2D, count: int)
 signal inventory_cleared(slot_index: int)
 
 # Currency
-var coins: int = 0
+var coins: int = 0:
+	set (value):
+		coin_changed.emit(coins)
 var keys: int = 0
 
 # Modules
@@ -56,7 +58,6 @@ func _ready() -> void:
 
 func add_coin(amount: int) -> void:
 	coins += amount
-	coin_changed.emit(coins)
 	item_collected.emit("coin", amount)
 	print("Collected ", amount, " coins. Total: ", coins)
 
